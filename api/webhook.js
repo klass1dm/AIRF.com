e153db7fef0354b13e99d2da1817c7aaeecdd804
klass1dm <klass1digitalmedia@gmail.com>
2025-10-14 19:47:32 -0700

    Update webhook.js
    Update to ES modules syntax

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 // Vercel Function for handling Stripe webhooks
 import Stripe from 'stripe';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+         const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
